10cf7e408df21f9c7333347e93b27753b5a49f24
Robin Andersson <danrobin.andersson@outlook.com>
2025-12-12 11:13:07 +0100

    Added basic structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/backend/vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yrgopelag</title>
+    <link rel="stylesheet" href="/frontend/styles/styles.css">
+</head>
+
+<body>
+
+
+    <script src="/frontend/scripts/scripts.js"></script>
+</body>
+
+</html>
